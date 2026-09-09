01546f4765352d8e411e70fcc3fc8e0c91ab7ece
Ivan Robles <irae45@gmail.com>
2026-09-09 12:48:50 -0600

    feat(client): add secrets encryption card to Settings.

--- client/src/components/pages/Store/Settings/Settings.jsx
+++ client/src/components/pages/Store/Settings/Settings.jsx
@@ -18,6 +18,7 @@ import { NwcConnectionCard } from "./NwcConnection/NwcConnectionCard";
 import { PhoenixdRemoteCard } from "./PhoenixdRemote/PhoenixdRemoteCard";
 import { Printers } from "./Printers";
 import { QRUrl } from "./QRUrl";
+import { SecretsEncryptionCard } from "./SecretsEncryption/SecretsEncryptionCard";
 import { SecureConnection } from "./SecureConnection/SecureConnection";
 import { Seed } from "./Seed";
 import { StoreInfo } from "./StoreInfo";
@@ -47,6 +48,7 @@ export function Settings() {
               <ImportData />
               <NwcConnectionCard />
               <PhoenixdRemoteCard />
+              <SecretsEncryptionCard />
               <Tutorials />
             </>
           )}
